5cce7d5b462be96c5826f089eb29a18ce0a25094
ChunLiang Wu <lucky0978218035@gmail.com>
2023-02-10 00:12:52 -0800

    add tokenize and lemmatization

--- data_utils.py
+++ data_utils.py
@@ -1,14 +1,58 @@
 import json
+import nltk
+import numpy as numpy
+from nltk.stem import WordNetLemmatizer
 
-def data_loading(data_root):
+def data_loading_json(data_root):
     """
-    sanity check for input string
+    data loading json file
     
     Input:
-    - s: str, folder path
+    - data_root: str, folder path
+
     Returns:
-    - s: str, folder path
+    - data: dictionary
     """
-    data_file = open(data_root + '/intents.json').read()
+    data_file = open(data_root + '/test.json').read()
     data = json.loads(data_file)
     return data
+
+def tokenize_and_lemmetaion(data):
+    """
+    tokenize, lemmetaion, delete punctuation, lower the word  
+    
+    Input:
+    - data: dict
+
+    Returns:
+    - words: set, include all lower and lemmatized word with no punctuation
+    - classes: set, include tag
+    - data_x: list, all the sentances
+    - data_y: list, all the tag
+    """
+    words = []
+    classes = []
+    data_x = []
+    data_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]: 
+            tokens = nltk.work_tokenize(pattern)
+            words.extend(tokens)
+            data_x.append(pattern)
+            data_y.append(intent["tag"])
+        
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+    
+    lemmatizer = WordNetLemmatizer()
+    # lemmatize all the words and delete punctuation
+    words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
+    # no duplicate and in alphabetical order
+    words = sorted(set(words))
+    classes = sorted(set(classes))
+
+    return words, classes, data_x, data_y
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import string 
+import random
+
+import nltk
+import numpy as numpy
+from nltk.stem import WordNetLemmatizer
+
+
+if __name__ == "__main__":
+    pass
