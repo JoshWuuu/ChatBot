efb37baaf34abb2e16f7c61625a92712c746b5ef
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-06 17:21:35 -0800

    Update model_build.py

--- generate-based-network/model_build.py
+++ generate-based-network/model_build.py
@@ -152,6 +152,101 @@ class LuongAttnDecoderRNN(nn.Module):
         # Return output and final hidden state
         return output, hidden
 
+class EncoderLSTM(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layer= 1, dropout = 0):
+        super(EncoderRNN, self).__init__()
+        self.n_layers = n_layer
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+
+        self.LSTM = nn.LSTM(hidden_size, hidden_size, n_layer, 
+        dropout=(0 if n_layer == 1 else dropout), bidirectional = True)
+    
+    def forward(self, input_seq, input_lengths, hidden = None):
+        """   
+        Input:
+        - input_seq: tensor, (max_length, batch_size)
+        - input_lengths: torch int, (batch_size)
+        - hidden: int, hidden state of the GRU
+
+        return shape:
+        - outputs: (max_length, batch_size, hidden states)
+        - hidden: (n_layer, num_directions, batch_size, hidden_size)
+        """
+        # Convert word indexes to embeddings
+        embedding = self.embedding(input_seq)
+         # Pack padded batch of sequences for RNN module, to improve computational efficiency
+        packed = nn.utils.rnn.pack_padded_sequence(embedding, input_lengths)
+        # Forward pass through GRU
+        outputs, hidden = self.LSTM(packed, hidden)
+        # Unpack padding
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs)
+        # Sum bidirectional GRU outputs
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:]
+         # Return output and final hidden state
+        return outputs, hidden
+
+class LuongAttnDecoderLSTM(nn.Module):
+    """
+    class for decoder
+        
+    Input:
+    - attn_model
+    - embedding: (batch_size, hidden_size)
+    - hidden_size: int
+    """
+    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(LuongAttnDecoderLSTM, self).__init__()
+
+        # Keep for reference
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # Define layers
+        self.embedding = embedding
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.LSTM = nn.LSTM(hidden_size, hidden_size, n_layers, dropout=(0 if n_layers == 1 else dropout))
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+        self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        """   
+        return shape:
+        - input_step: (1, batch_size)
+        - last_hidden: (n_layer, num_directions, batch_size, hidden_size)
+        - encoder_outputs: (max_length, batch_size, hidden_size)
+        - output: (batch_size, voc.num_words)
+        - hidden: (n_layer, num_directions, batch_size, hidden_size)
+        """
+        # Note: we run this one step (word) at a time
+        # Get embedding of current input word
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded) # (1, batch_size, hidden_state)
+        # Forward through unidirectional GRU
+        rnn_output, hidden = self.LSTM(embedded, last_hidden) # rnn_output: (1, batch_size, hidden_state)
+        # Calculate attention weights from the current GRU output
+        attn_weights = self.attn(rnn_output, encoder_outputs) # rnn_output: (1, batch_size, hidden_state), (max_length, batch_size, hidden_size)
+        # attn_weights [batch size, 1, max_length]
+        # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+        # context (batch_size, 1, hidden state)
+        # Concatenate weighted context vector and GRU output using Luong eq. 5
+        rnn_output = rnn_output.squeeze(0) # rnn_output: (batch_size, hidden_state)
+        context = context.squeeze(1) # context: (batch_size, hidden_state)
+        concat_input = torch.cat((rnn_output, context), 1) # concat_input: (batch_size, 2* hidden_state)
+        concat_output = torch.tanh(self.concat(concat_input)) # concat_input : (batch_size, hidden_state)
+        # Predict next word using Luong eq. 6
+        output = self.out(concat_output) 
+        output = F.softmax(output, dim=1)
+        # Return output and final hidden state
+        return output, hidden
+    
+
 def main():
     pass
 
