88fc8032e1c81709219bc35e3ef2c4aafcda3485
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-12 22:36:44 -0700

    Update main.py

--- generate-based-network/main.py
+++ generate-based-network/main.py
@@ -52,36 +52,61 @@ def main():
     save_model = True
 
     # Training hyperparameters
-    num_epochs = 100
+    num_epochs = 2
     learning_rate = 3e-4
-    batch_size = 64
-
-    model_name = 0
+    batch_size = 2
+    model_name = 'transformer'
+    
     # Model hyperparameters
-    src_vocab_size = len(vocab)
-    trg_vocab_size = len(vocab)
-    embedding_size = 512
-    num_heads = 8
-    num_encoder_layers = 3
-    num_decoder_layers = 3
-    dropout = 0.10
-    max_len = 100
-    forward_expansion = 4
-    src_pad_idx = vocab.stoi["<pad>"]
+    # src_vocab_size = len(vocab)
+    # trg_vocab_size = len(vocab)
+    # embedding_size = 512
+    # num_heads = 8
+    # num_encoder_layers = 3
+    # num_decoder_layers = 3
+    # dropout = 0.10
+    # max_len = 100
+    # forward_expansion = 4
+    # src_pad_idx = vocab.stoi["<pad>"]
 
-    model = Transformer(
-        embedding_size,
-        src_vocab_size,
-        trg_vocab_size,
-        src_pad_idx,
-        num_heads,
-        num_encoder_layers,
-        num_decoder_layers,
-        forward_expansion,
-        dropout,
-        max_len,
-        device,
+    # model = Transformer(
+    #     embedding_size,
+    #     src_vocab_size,
+    #     trg_vocab_size,
+    #     src_pad_idx,
+    #     num_heads,
+    #     num_encoder_layers,
+    #     num_decoder_layers,
+    #     forward_expansion,
+    #     dropout,
+    #     max_len,
+    #     device,
+    # ).to(device)
+
+    # Model hyperparameters
+    input_size_encoder = len(vocab)
+    input_size_decoder = len(vocab)
+    output_size = len(vocab)
+    encoder_embedding_size = 300
+    decoder_embedding_size = 300
+    hidden_size = 1024
+    num_layers = 1
+    enc_dropout = 0.0
+    dec_dropout = 0.0
+    encoder_net = Encoder(
+    input_size_encoder, encoder_embedding_size, hidden_size, num_layers, enc_dropout
+).to(device)
+
+    decoder_net = Decoder(
+        input_size_decoder,
+        decoder_embedding_size,
+        hidden_size,
+        output_size,
+        num_layers,
+        dec_dropout,
     ).to(device)
+
+    model = Seq2Seq(encoder_net, decoder_net, vocab).to(device)
 
     train_dataloader = DataLoader(pairdataset, batch_sampler=batch_sampler_fn(pairs, batch_size), 
                               collate_fn=collate_batch)
